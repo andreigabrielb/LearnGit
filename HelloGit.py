123ba2d050692a418dd5513a35a5a35fc45cd771
Bochis <bochisandrei@ad.garmin.com>
2018-06-08 14:40:41 +0300

    First file upload test

---
+++ HelloGit.py
@@ -0,0 +1,24 @@
+""" 
+Ok let us see if this work.
+
+This will document the code that i would write
+
+Including comments
+
+Args:
+    a =  definition 1
+    b = definition 2
+Returns 
+    whatever
+"""
+
+def plus1(arg):
+    ***
+    this is a test function
+    ***
+    print "Function was called"
+    return arg + 1
+
+a = 5
+print ("Hello Git! ")
+print plus1(a)
